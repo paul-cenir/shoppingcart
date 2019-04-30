dcd7e842be9ee70757c7a86860294e02c2894f2d
paulcenir003 <paul.cenir03@gmail.com>
2019-04-30 18:25:42 +0800

    for updates

--- src/app/shared-module/api-services/product/product-api.service.ts
+++ src/app/shared-module/api-services/product/product-api.service.ts
@@ -1,7 +1,7 @@
 import { Product } from '../product/product';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 import { environment } from '../../../../environments/environment';
 
@@ -20,7 +20,13 @@ export class ProductApiService {
 
     /** GET heroes from the server */
     getProducts(): Observable<Product[]> {
-        return this.http.get<Product[]>(environment.apiUrl + 'product');
+        return this.http.get<Product[]>(environment.apiUrl + 'product')
+            .pipe(
+                catchError(err => {
+                    console.log('Handling error locally and rethrowing it...', err);
+                    return throwError(err);
+                })
+            );
     }
 
     getProduct(id: number): Observable<Product> {
